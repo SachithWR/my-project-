ef863243c92703218867bc296c2a1c393096d05c
SachithWR <113272779+SachithWR@users.noreply.github.com>
2022-10-09 21:52:17 +0530

    Added some views
    Home and About views added

--- hospital_management_system/hospital_mngt/hospital/views.py
+++ hospital_management_system/hospital_mngt/hospital/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def About(request):
-    return HttpResponse("this is the about section")
+    return render(request,'about.html')
+
+def Home(request):
+    return render(request,'home.html')
