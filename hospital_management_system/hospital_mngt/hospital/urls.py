5cf5dd7ef67ed423e1bfd8062fd02c7c6b0e8cc7
Sandupa22 <113241727+Sandupa22@users.noreply.github.com>
2022-10-12 19:46:12 +0530

    initial commit

--- hospital_management_system/hospital_mngt/hospital/urls.py
+++ hospital_management_system/hospital_mngt/hospital/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import About,Home
+from .views import About,Home,Contact
 
 urlpatterns = [
     path('', Home, name='home'),
     path('about/', About, name='about'),
+    path('contact/', Contact, name='contact'),
 ]
