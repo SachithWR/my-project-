845e28fdb895f86ed097541eeaf77b27be3dfd93
Sandupa22 <113241727+Sandupa22@users.noreply.github.com>
2022-10-14 14:50:51 +0530

    Update models.py

--- hospital_management_system/hospital_mngt/hospital/models.py
+++ hospital_management_system/hospital_mngt/hospital/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    Name = models.chharField(max_length=50)
+    mobile = models.InregerField()
+    special = models.Chsarfield(max_length=50)
+
+
+
+class patient(models.Model):
+    name = models.Charfield(max_length=50)
+    gender = models.CharField(max_length=10)
+    mobile = models.IntegerField(null=True)
+    address = models.TextField()
+
+
+class Appoinment(models.Model):
+    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    patient = models.ForeignKey(patient,on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
+
