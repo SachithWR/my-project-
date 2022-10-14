b5cc6ea21edbf33111ca5fd68e4ca5dc161b4ad3
Sandupa22 <113241727+Sandupa22@users.noreply.github.com>
2022-10-14 15:29:05 +0530

    Update admin.py

--- hospital_management_system/hospital_mngt/hospital/admin.py
+++ hospital_management_system/hospital_mngt/hospital/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from hospital_mngt.hospital.models import Appoinment, Doctor, Patient
+from .models import Appoinment, Doctor, Patient
 
 # Register your models here.
 admin.site.register(Doctor)
